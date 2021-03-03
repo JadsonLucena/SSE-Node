30b6c8bb150af8da6c6170a05bf486b976604fdc
JadsonLucena <jadsonlucena@gmail.com>
2021-03-03 18:07:47 -0300

    feat (exceptions): http status errors
    1. The request type must be text/event-stream
    2. Each public IP has a connection limit
    3. The origin must have permission

--- src/SSE.js
+++ src/SSE.js
@@ -2,11 +2,58 @@ const EventEmitter = require('events');
 
 class SSE extends EventEmitter {
 
-    constructor() {
+    #allowOrigin;
+    #clients;
+    #limitByIP;
+    #path;
+    #withCredentials;
+
+    constructor(server, {
+        allowOrigin = null, // The value should be similar to what Access-Control-Allow-Origin would receive
+        limitByIP = 256, // IP connection limit (Must be greater than zero)
+        path = '/sse', // Path using in route
+        withCredentials = false // Tells whether or not the client should use credentials
+    } = {}) {
 
         super({captureRejections: true});
 
         this.setMaxListeners(0);
+
+        this.#allowOrigin = allowOrigin;
+        this.#clients = {};
+        this.#limitByIP = limitByIP;
+        this.#path = path;
+        this.#withCredentials = withCredentials;
+
+        server.on('request', (request, response) => {
+
+            if (request.method == 'GET' && request.path == this.#path) {
+                
+                if (request.headers['accept'] != 'text/event-stream') {
+
+                    response.status(406).send('Not Acceptable');
+                    response.end();
+                    response.destroy();
+
+                } else if (this.#limitByIP >= 1 && Object.keys(this.#clients).filter(clientId => this.#clients[clientId].connection.remoteAddress == request.connection.remoteAddress).length + 1 > this.#limitByIP) {
+
+                    response.status(429).send('Too Many Requests');
+                    response.end();
+                    response.destroy();
+
+                } else {
+
+                    if (this.#allowOrigin && request.headers['origin'] && (this.#allowOrigin == '*' || this.#allowOrigin.includes(request.headers['origin']))) {
+
+                        response.setHeader('Access-Control-Allow-Origin', request.headers['origin']);
+
+                    }
+
+                }
+
+            }
+
+        });
 
     }
 
